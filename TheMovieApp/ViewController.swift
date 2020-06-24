4c412b5415b162d5f7901940fb9ad2bfaa254fc1
Nitin <>
2020-06-24 22:24:28 +0530

    Initial Setup

---
+++ TheMovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieApp
+//
+//  Created by Nitz on 24/06/20.
+//  Copyright © 2020 TM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
